250c12740a78cc0f5d1427cee53c8d2905df4ed3
Jerrybird <jerryzvs@163.com>
2020-08-24 09:16:31 -0400

    添加plan文件

--- README.md
+++ README.md
@@ -12,22 +12,20 @@ go run main.go
 (二) 使用二进制文件
 git clone https://github.com/Chroblert/JCRandomProxy.git
 将适合自己系统的二进制文件拷贝到clone下来的目录下
-执行：
 
 使用说明：
-该工具需与proxypool配合使用，或者将代理IP写在proxy.lst文件中
+该工具需与proxypool配合使用，或者将确定可以使用的代理IP写在proxy.lst文件中
 
-命令说明：
+配置说明：
+参见config.ini
 
 目录说明：
 Conf：
-- config.go
-- config.ini
-- proxy.lst
+- config.go # 配置相关
+- config.ini # 配置文件
+- proxy.lst # 确定可用的代理
 Proxy：
-- GetAProxy.go
-- GetAProxyA.go
-- GetAProxyB.go
-JCLog:
-- JCLog.go
+- GetAProxy.go # 获取代理
+- GetAProxyA.go # 从proxypool中获取代理
+- GetAProxyB.go # 从proxy.lst中获取代理
 main.go
--- main.go
+++ main.go
@@ -1,47 +1,54 @@
 package main
 
 import (
+	"JCRandomProxy/Conf"
+	"JCRandomProxy/Proxy"
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"runtime/debug"
-	"bytes"
 	"strings"
-	"fmt"
-	"bufio"
 	"time"
-	"io"
-	"io/ioutil"
-	"JCRandomProxy/Conf"
-	"JCRandomProxy/Proxy"
 )
 
-func init(){
-	log.SetFlags(log.LstdFlags|log.Lshortfile)
+/**
+*
+* Author: JC0o0l
+* email: jerryzvs@163.com
+* wechat: JC_SecNotes
+ */
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	Conf.InitConfig()
 }
 
-func main(){
+func main() {
 	// 监听TCP连接
-	l,err := net.Listen("tcp",":"+Conf.Port)
+	l, err := net.Listen("tcp", ":"+Conf.Port)
 	if err != nil {
 		log.Panic(err)
 	}
 
 	for {
 		// 接收TCP连接，返回一个net.Conn
-		client,err := l.Accept()
+		client, err := l.Accept()
 		if err != nil {
-			log.Panic("Panic",err)
+			log.Panic("Panic", err)
 		}
 		// 收到请求后，调用handle进行处理
 		go handle(client)
 	}
 }
 
-func handle(client net.Conn){
-	defer func(){
+func handle(client net.Conn) {
+	defer func() {
 		if err := recover(); err != nil {
 			log.Panic(err)
 			debug.PrintStack()
@@ -51,21 +58,21 @@ func handle(client net.Conn){
 		return
 	}
 
-	log.Println("JCTLog: client tcp tunnel connection: ",client.LocalAddr().String(),"->",client.RemoteAddr().String())
+	log.Println("JCTLog: client tcp tunnel connection: ", client.LocalAddr().String(), "->", client.RemoteAddr().String())
 	defer client.Close()
 
 	var b [1024]byte
 	// 读取应用层的所有数据
-	n,err := client.Read(b[:])
-	if err != nil || bytes.IndexByte(b[:],'\n') == -1 {
+	n, err := client.Read(b[:])
+	if err != nil || bytes.IndexByte(b[:], '\n') == -1 {
 		// 传输层的连接没有应用层的内容，如net.Dial()
 		log.Println(err)
 		return
 	}
-	var method,host,address string
-	fmt.Sscanf(string(b[:bytes.IndexByte(b[:],'\n')]),"%s%s",&method,&host)
-	log.Println(method,host)
-	hostPortURL,err := url.Parse(host)
+	var method, host, address string
+	fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
+	log.Println(method, host)
+	hostPortURL, err := url.Parse(host)
 	if err != nil {
 		log.Println(err)
 		return
@@ -73,41 +80,41 @@ func handle(client net.Conn){
 	// https
 	if hostPortURL.Opaque == "443" {
 		address = hostPortURL.Scheme + ":443"
-	}else{
+	} else {
 		// http
-		if strings.Index(hostPortURL.Host,":") == -1{
+		if strings.Index(hostPortURL.Host, ":") == -1 {
 			address = hostPortURL.Host + ":80"
-		}else{
+		} else {
 			address = hostPortURL.Host
 		}
 	}
-	log.Println("JCTLog: hostPortURL",address)
+	log.Println("JCTLog: hostPortURL", address)
 	// 建立一个到代理服务器的传输通道
-	server,err := Dial("tcp",address)
+	server, err := Dial("tcp", address)
 	if err != nil {
-		log.Println("JCTLog: Dial: ",err)
+		log.Println("JCTLog: Dial: ", err)
 		return
 	}
 	// 在应用层完成数据转发后，关闭传输层的通道
 	defer server.Close()
-	log.Println("JCTLog: server tcp tunnel connection: ",server.LocalAddr().String(),"->",server.RemoteAddr().String())
-	
+	log.Println("JCTLog: server tcp tunnel connection: ", server.LocalAddr().String(), "->", server.RemoteAddr().String())
+
 	if method == "CONNECT" {
 		// https
-		fmt.Fprint(client,"HTTP/1.1 200 Connection established\r\n\r\n")
-	}else{
+		fmt.Fprint(client, "HTTP/1.1 200 Connection Established\r\n\r\n")
+	} else {
 		// http
-		log.Println("JCTLog: ","server write ",method)
+		log.Println("JCTLog: ", "server write ", method)
 		server.Write(b[:n])
 	}
 	// 进行转发
-	go func(){
+	go func() {
 		log.Println("JCTLog: go转发前：")
-		io.Copy(server,client)
+		io.Copy(server, client)
 		log.Println("JCTLog: go转发后：")
 	}()
 	log.Println("JCTLog: 开始转发：")
-	io.Copy(client,server) // 
+	io.Copy(client, server) //
 	// var tt []byte
 	// tt,err = ioutil.ReadAll(server)
 	// log.Println("ddddd",string(tt))
@@ -118,77 +125,79 @@ func handle(client net.Conn){
 // 建立一个传输通道
 // network : 网络类型，tcp
 // addr: 最终目标服务器地址
-func Dial(network,addr string) (net.Conn,error){
+func Dial(network, addr string) (net.Conn, error) {
 	var proxyAddr string
 	proxyAddr = "http://10.103.90.8:10080"
 	proxyAddr = "http://49.4.123.243:8080"
 	// 随机取出一个代理
-	paddr,ptype,_ := Proxy.GetAProxy()
+	paddr, ptype, _ := Proxy.GetAProxy()
 	proxyAddr = ptype + "://" + paddr
 
 	// 建立到代理服务器的传输层通道
-	c,err := func() (net.Conn,error){
-		prox,_ := url.Parse(proxyAddr)
-		log.Println("JCTLog: 代理地址: ",prox.Host)
+	c, err := func() (net.Conn, error) {
+		prox, _ := url.Parse(proxyAddr)
+		log.Println("JCTLog: 代理地址: ", prox.Host)
 		// Dial and create client connection
-		proxc,err := net.DialTimeout("tcp",prox.Host,time.Second * 5)
+		proxc, err := net.DialTimeout("tcp", prox.Host, time.Second*5)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 		// 在这里返回c可以正常使用
 		checkaddr := "http://myip.ipip.net"
-		if CheckProxy(proxyAddr,checkaddr) {
-			return proxc,err
+		if CheckProxy(proxyAddr, checkaddr) {
+			return proxc, err
 		}
-		return nil,err
-		
+		return nil, err
+
 	}()
 	if c == nil || err != nil {
-		log.Println("JCTLog: 代理异常: ",c,err)
+		log.Println("JCTLog: 代理异常: ", c, err)
 		// log.Println("JCTLog: 本地直接转发: ")
-		return net.Dial(network,addr)
+		return net.Dial(network, addr)
 	}
-	log.Println("JCTLog: 代理正常，tunnel信息 ",c.LocalAddr().String(),"->",c.RemoteAddr().String())
-	return c,err
+	log.Println("JCTLog: 代理正常，tunnel信息 ", c.LocalAddr().String(), "->", c.RemoteAddr().String())
+	return c, err
 }
 
 // 验证代理服务器是否可用
-func CheckProxy(proxyAddr,checkaddr string) bool{
-	// return true
-	prox,_ := url.Parse(proxyAddr)
-	log.Println("JCTLog: 代理地址: ",prox.Host)
+func CheckProxy(proxyAddr, checkaddr string) bool {
+	if !Conf.UseProxyPool {
+		return true
+	}
+	prox, _ := url.Parse(proxyAddr)
+	log.Println("JCTLog: 代理地址: ", prox.Host)
 	// Dial and create client connection
-	proxc,err := net.DialTimeout("tcp",prox.Host,time.Second * 5)
+	proxc, err := net.DialTimeout("tcp", prox.Host, time.Second*5)
 	if err != nil {
 		return false
 	}
 	// 解析最终目标url
-	reqURL ,err := url.Parse(checkaddr)
+	reqURL, err := url.Parse(checkaddr)
 	if err != nil {
 		return false
 	}
-	log.Println("JCTLog: reqURL: " ,reqURL.String())
-	req,err := http.NewRequest(http.MethodGet,reqURL.String(),nil)
+	log.Println("JCTLog: reqURL: ", reqURL.String())
+	req, err := http.NewRequest(http.MethodGet, reqURL.String(), nil)
 	if err != nil {
-		log.Println("JCTLog: http.NewRequest: ",err)
+		log.Println("JCTLog: http.NewRequest: ", err)
 		return false
 	}
 
 	req.Close = false
-	req.Header.Set("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.3")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.3")
 	err = req.Write(proxc)
 	fmt.Println(req)
 	if err != nil {
-		log.Println("JCTLog: req.Write: ",err)
+		log.Println("JCTLog: req.Write: ", err)
 		return false
 	}
-	
-	resp,err := http.ReadResponse(bufio.NewReader(proxc),req)
+
+	resp, err := http.ReadResponse(bufio.NewReader(proxc), req)
 	if err != nil {
-		log.Println("JCTLog: http.ReadResponse: ",err)
+		log.Println("JCTLog: http.ReadResponse: ", err)
 		return false
 	}
-	defer resp.Body.Close()	
+	defer resp.Body.Close()
 	fmt.Println("===================sss")
 	// fmt.Println(resp.Body)
 	// fmt.Println(resp.StatusCode)
@@ -204,16 +213,16 @@ func CheckProxy(proxyAddr,checkaddr string) bool{
 
 	// fmt.Println(string(body))
 	fmt.Println("===================eee")
-	defer resp.Body.Close()		
+	defer resp.Body.Close()
 	// fmt.Println(strings.Split(prox.Host,":")[1])
-	if strings.Contains(string(body),strings.Split(prox.Host,":")[0]) {
-		fmt.Println("包含",prox.Host)
+	if strings.Contains(string(body), strings.Split(prox.Host, ":")[0]) {
+		fmt.Println("包含", prox.Host)
 		return true
 	}
 	// 删除无效代理
 	if Conf.UseProxyPool {
-		_,err := http.Get( Conf.PPIP + ":" + Conf.PPPort + "/delete/?proxy="+prox.Host)
-		if err != nil{
+		_, err := http.Get(Conf.PPIP + ":" + Conf.PPPort + "/delete/?proxy=" + prox.Host)
+		if err != nil {
 			log.Println(err)
 		}
 		// body, err := ioutil.ReadAll(resp.Body)
@@ -221,11 +230,11 @@ func CheckProxy(proxyAddr,checkaddr string) bool{
 		// 	fmt.Println(err)
 		// 	return false
 		// }
-		log.Println("JCTLog: 删除代理: ",prox.Host)
+		log.Println("JCTLog: 删除代理: ", prox.Host)
 	}
 	// if (resp.StatusCode != 200) {
-		err = fmt.Errorf("Connect server using proxy error,StatusCode [%d]",resp.StatusCode)
-		return false
+	err = fmt.Errorf("Connect server using proxy error,StatusCode [%d]", resp.StatusCode)
+	return false
 	// }
 
 }
